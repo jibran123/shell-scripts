7e486d14018da7d27a9557c56fc642b34388a718
jibran123 <pateljibran2008@gmail.com>
2017-02-11 16:46:47 +0530

    Updated. Added error handling

--- ssh_server.sh
+++ ssh_server.sh
@@ -1,5 +1,11 @@
 #Script assumes that all hosts passed as a parameter to it, have the same user and same private key authentication file
 hosts=$1
+#If no command is passed at prompt 'hostname' command will execute by default
+if [ -z "$1" ]
+  then
+    echo "No hostname mentioned as parameter. Please execute the script again and pass the hostname."
+    exit 1    
+fi
 IFS=', ' read -r -a array <<< $hosts
 echo "Welcome!! Enter command."
 echo "Note: The command will be executed on all the hosts mentioned above."
@@ -8,12 +14,18 @@ echo "Type 'exit' to exit the promt. Thank You."
 read text
 while [ "$(echo $text | tr '[:upper:]' '[:lower:]')" != "exit" ]
 do
+  if [ -z "$text" ]
+    then
+      echo "No Command mentioned. The script will execute 'hostname' command by default"
+      text="hostname"    
+  fi
   echo "'$text' will be executed on all the hosts mentioned in the parameter."
   for element in "${array[@]}"
   do
     #Adding sleep in order to catch up in case of large amount of data in output
     sleep 2s
     echo "Executing in '$element' host. Output as follows - "
+    ssh -i "/tmp/aws.pem" ec2-user@$element $text
     # Authentication is done via ec2-user, private key authentication file is aws.pem Which is assumed to be common for all the hosts.
   done
   echo "Enter next command. To stop type 'exit'."
